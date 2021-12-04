e8e317db9907c9f8fadf195d2ecb269e9907a9cb
Hoesung <jrps2212@gmail.com>
2021-12-04 14:33:34 +0900

    live coding

--- calc_practice.py
+++ calc_practice.py
@@ -6,3 +6,19 @@
 
 # 1
 # 15
+
+remain = 1000 - int(input())
+coins = [500, 100, 50, 10, 5, 1]
+
+whole_coin_count = 0
+
+for coin in coins:
+  # coin_count = remain//coin
+  # whole_coin_count += coin_count
+  
+  whole_coin_count += remain//coin
+  
+  # remain = remain - coin_count * coin
+  remain %= coin
+
+print(whole_coin_count)
