6846f5b377ffaa78a689bb385f395aa7c31c6db7
Hoesung Jung <jrps2212@gmail.com>
2021-12-08 14:35:18 +0900

    recommned

---
+++ recommend_new_id.py
@@ -0,0 +1,18 @@
+# https://programmers.co.kr/learn/courses/30/lessons/72410?language=python3
+# 위 링크의 문제를 해결할 것.
+
+# 힌트 1: String type의 변수에 .lower() 메서드(객체 안의 함수를 메서드라고 부른다)를 사용하면 해당 변수의 문자열이 소문자로 바뀐다. 
+
+# 힌트 2: String type의 변수에 .join( 문자열로 바꿀 리스트 변수 ) 메서드를 사용하면 List type 변수 안의 값들을 String type 변수를 사이사이에 껴서 String type 변수로 반환한다. 
+# https://blockdmask.tistory.com/468 참고
+
+# 힌트 3: tring type의 변수에 .strip( 지울 문자 변수 ) 메서드를 사용하면, 문자열의 왼쪽과 오른쪽에서 해당 문자열을 제거한다. lstrip은 왼쪽만, rstrip은 오른쪽만 제거한다.
+
+# 다음과 같은 new_id 에서는 "bat.y.abcdefghi" 가 출력되어야 한다. 
+
+new_id = "...!@BaT#*.y.abcdefghijklm"
+
+def solution(new_id):
+  return 
+
+print(solution(new_id))
