99bdfbacf296685fa68402c0e46bb08b30c06829
Hoesung Jung <jrps2212@gmail.com>
2021-12-08 14:23:07 +0900

    multiply or plus update

---
+++ multiply_or_plus.py
@@ -0,0 +1,11 @@
+# 숫자로 구성된 문자열 data가 주어진다. data의 각 자리수를 앞에서부터 더하거나 곱할 수 있다. 
+# 이때 최대의 값을 반환하는 multiply_or_plus 함수를 완성하라.
+# 14312512일 때, 322가 반환되어야 한다. 
+
+def multiply_or_plus(data):
+
+  return 
+
+data = "14312512"
+
+print(multiply_or_plus(data))
